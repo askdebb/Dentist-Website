4e81683195d78cf90ae6aa59779c1a4271064f09
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-07-14 00:34:53 +0000

    worked on the css for home.html and activated the

--- dentist_portfolio/dentist_Site/urls.py
+++ dentist_portfolio/dentist_Site/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns =[
     path('',views.homePage,name="homePage"), 
+    path('contact.html/',views.contactPage,name="contactPage"), 
 ]
--- dentist_portfolio/dentist_Site/views.py
+++ dentist_portfolio/dentist_Site/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def homePage(request):
     return render(request,'home.html',{})
+
+def contactPage(request):
+    if request. == 'post':
+        do stuff
+    else:   
+        return render(request,'contact.html',{})
